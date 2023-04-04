146b03e9d405807620ce05e37beac61485f15d3d
kn4y4n <kn4y4nnn@gmail.com>
2023-04-05 01:17:07 +0530

    feat: create custom user model

---
+++ accounts/managers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Email field is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, password=password, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_verified', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff set True")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser set True")
+        
+        return self.create_user(email=email, password=password, **extra_fields)
